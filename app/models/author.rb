58c9f28a6794c347c44dfd8c44fe85a482d166ac
chrstno040420 <104917222+chrstno040420@users.noreply.github.com>
2022-08-17 16:43:57 -0500

    commit tree 8cc92d86021bb3d0e321f066963e1cef29939581

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,18 @@
 class Author < ApplicationRecord
+    # 1. All authors have a name
+    # 2. No two authors have the same name
+    # 3. Author phone numbers are exactly ten digits
+    # 4. All posts have a title
+    # 5. Post content is at least 250 characters long
+    # 6. Post summary is a maximum of 250 characters
+    # 7. Post category is either `Fiction` or `Non-Fiction`. This step requires an
+    #    `inclusion` validator, which was not outlined in the lesson. You'll need to
+    #    refer to the [Rails guide][active record validations] to look up how to use
+    #    it.
+    # create_table :authors do |t|
+    #     t.string :name
+    #     t.string :phone_number
+    validates :name , presence: true , uniqueness: { case_sensitive: true}
+    validates :phone_number , length: {is:10}
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+
+    # create_table :posts do |t|
+    #     t.string :title
+    #     t.text :content
+    #     t.text :summary
+    #     t.string :category
+  
+    validates :title , presence: true , exclusion:{in:["True Facts"]}
+    validates :content , length:{minimum: 250}
+    validates :summary , length:{maximum:250}
+    validates :category , presence: true , inclusion: {in:["Fiction" , "Non-Fiction"]}
+
+
 end
